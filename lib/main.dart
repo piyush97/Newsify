aa7afec0d7152f0425d852780bb6c0ad860bb2a5
Piyush Mehta <me@piyushmehta.com>
2018-07-15 23:38:27 +0530

    New Fetching done
    Signed-off-by: Piyush Mehta <me@piyushmehta.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 import "package:flutter/material.dart" ;
 
@@ -25,7 +26,19 @@ class HomePageState extends State<HomePage>{
   Future<String> getJsonData() async{
     var response = await http.get(
 //encoding the url
+Uri.encodeFull(url),
+//only accept json response
+headers: {'accept':'application/json'}
+
     );
+
+    print(response.body);
+    setState((){
+      var convertDataToJson = JSON.decode(response.body);
+      data = convertDataToJson['articles'];
+    });
+
+    return 'Success!!!! yayyy';
   }
   @override
   Widget build(BuildContext context){
@@ -34,7 +47,7 @@ class HomePageState extends State<HomePage>{
         title:new Text("Newsify"),
         ) ,
         body: new ListView.builder(
-          itemCount: 1,
+          itemCount: data == null ? 0 : data.length,
           itemBuilder: (BuildContext context,int index){
       return new Container(
 child: new Center(
@@ -43,7 +56,7 @@ child: new Center(
   children: <Widget>[
     new Card(
       child:new Container(
-        child: new Text("News Card"),
+        child: new Text(data[index]['title']),
         padding:const EdgeInsets.all(20.0)
       ),
     )

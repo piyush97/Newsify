c7c08d2ff761cfdd677d40ceee470e89e4d44912
Piyush Mehta <me@piyushmehta.com>
2018-07-15 23:28:03 +0530

    Fetcher

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+import 'package:http/http.dart' as http;
 import "package:flutter/material.dart" ;
 
 void main() =>runApp(new MaterialApp(
@@ -11,6 +13,20 @@ HomePageState createState()=> new HomePageState();
 }
 
 class HomePageState extends State<HomePage>{
+  final String url = "https://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=5915defd1d264ab489add55da5bc6552";
+  List data;
+  @override
+  void initState(){
+    super.initState();
+    this.getJsonData();
+
+  }
+  //Similar to Promises in ES 6 and like async await
+  Future<String> getJsonData() async{
+    var response = await http.get(
+//encoding the url
+    );
+  }
   @override
   Widget build(BuildContext context){
     return new Scaffold(
@@ -27,7 +43,7 @@ child: new Center(
   children: <Widget>[
     new Card(
       child:new Container(
-        child: new Text("Hello"),
+        child: new Text("News Card"),
         padding:const EdgeInsets.all(20.0)
       ),
     )
